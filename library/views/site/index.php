f97356d9272da3e08871980977c73d220b45325c
Ersan <Ersan_a_s.97@mail.ru>
2020-06-05 13:33:35 +0600

    ***

--- library/views/site/index.php
+++ library/views/site/index.php
@@ -14,34 +14,32 @@
 $this->title = $settings[\common\enums\SettingsEnum::LIBRARY_BRAND_LABEL];
 ?>
 
-<div class="site-index">
-    <?= $this->render('include/_authorization_block', [
-        'totalUsers' => $totalUsers,
-        'librarySpace' => $settings[\common\enums\SettingsEnum::LIBRARY_SPACE_INFO],
-        'libraryFond' => $settings[\common\enums\SettingsEnum::LIBRARY_FOND_INFO],
-        'background' => $authBlockBackground,
+<?= $this->render('include/_authorization_block', [
+    'totalUsers' => $totalUsers,
+    'librarySpace' => $settings[\common\enums\SettingsEnum::LIBRARY_SPACE_INFO],
+    'libraryFond' => $settings[\common\enums\SettingsEnum::LIBRARY_FOND_INFO],
+    'background' => $authBlockBackground,
+]) ?>
+<main class="site_content">
+    <?= $this->render('include/_slider') ?>
+    <?= $this->render('include/_smart_spaces', [
+        'dataProvider' => $smartSpacesProvider,
+        'map' => $smartSpacesMap,
     ]) ?>
-    <main class="site_content">
-        <?= $this->render('include/_slider') ?>
-        <?= $this->render('include/_smart_spaces', [
-            'dataProvider' => $smartSpacesProvider,
-            'map' => $smartSpacesMap,
-        ]) ?>
-        <?= $this->render('include/_additional_sections', [
-            'dataProvider' => $additionalSections,
-        ]) ?>
-        <?= $this->render('include/_renewal_application', [
-            'model' => $renewalApplication,
-        ]) ?>
-        <?= $this->render('include/_map', [
-            'longitude' => $settings[\common\enums\SettingsEnum::LIBRARY_MAP_LONGITUDE],
-            'latitude' => $settings[\common\enums\SettingsEnum::LIBRARY_MAP_LATITUDE]
-        ]) ?>
-        <?= $this->render('include/_questions', [
-            'dataProvider' => $questionsProvider,
-        ]) ?>
-        <?= $this->render('include/_feedback' , [
-            'dataProvider' => $feedbackProvider,
-        ])?>
-    </main>
-</div>
+    <?= $this->render('include/_additional_sections', [
+        'dataProvider' => $additionalSections,
+    ]) ?>
+    <?= $this->render('include/_renewal_application', [
+        'model' => $renewalApplication,
+    ]) ?>
+    <?= $this->render('include/_map', [
+        'longitude' => $settings[\common\enums\SettingsEnum::LIBRARY_MAP_LONGITUDE],
+        'latitude' => $settings[\common\enums\SettingsEnum::LIBRARY_MAP_LATITUDE]
+    ]) ?>
+    <?= $this->render('include/_questions', [
+        'dataProvider' => $questionsProvider,
+    ]) ?>
+    <?= $this->render('include/_feedback', [
+        'dataProvider' => $feedbackProvider,
+    ]) ?>
+</main>
